993ed6de1136b7fba647a114501899aa41d61561
Lucas Segurado <segurado1996@gmail.com>
2020-07-21 20:29:58 -0300

    Lint commit

--- src/LSElement/properties/addReduxStores.ts
+++ src/LSElement/properties/addReduxStores.ts
@@ -1,14 +1,14 @@
-import type { LSCustomElement } from "../types";
-import { updateChangesInDom } from "../render/updateChangesInDom";
+import type { LSCustomElement } from '../types';
+import { updateChangesInDom } from '../render/updateChangesInDom';
 
 export function addReduxStores(self: LSCustomElement) {
-    self.lsStatic.stores.forEach(storeProperty => {
-        self[storeProperty.propertyName] = storeProperty.store.getState();
-        storeProperty.store.subscribe(() => {
-            if (self.ls.alreadyConnected) {
-                updateChangesInDom(self);
-                self[storeProperty.propertyName] = storeProperty.store.getState();
-            }
-        })
-    })
+	self.lsStatic.stores.forEach(storeProperty => {
+		self[storeProperty.propertyName] = storeProperty.store.getState();
+		storeProperty.store.subscribe(() => {
+			if (self.ls.alreadyConnected) {
+				updateChangesInDom(self);
+				self[storeProperty.propertyName] = storeProperty.store.getState();
+			}
+		});
+	});
 }
