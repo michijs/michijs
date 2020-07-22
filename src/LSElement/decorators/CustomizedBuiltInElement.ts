1515b29be530e09f95318c7549e699c413434dbd
Lucas Segurado <segurado1996@gmail.com>
2020-07-22 13:05:53 -0300

    Linter fixes

--- src/LSElement/decorators/CustomizedBuiltInElement.ts
+++ src/LSElement/decorators/CustomizedBuiltInElement.ts
@@ -16,61 +16,61 @@ interface CustomizedBuiltInElement {
 }
 
 const validateTag = (tag: string) => {
-    if (tag.indexOf('-') <= 0) {
-        throw new Error('You need at least 1 dash in the custom element name!');
-    }
+	if (tag.indexOf('-') <= 0) {
+		throw new Error('You need at least 1 dash in the custom element name!');
+	}
 };
 
 export const CustomizedBuiltInElement = (config: CustomizedBuiltInElement) => (element: CustomElementConstructor) => {
-    const tag = config?.tag || formatToLowerCase(element.name);
-    validateTag(tag);
+	const tag = config?.tag || formatToLowerCase(element.name);
+	validateTag(tag);
 
-    const emptyFunction = () => { };
-    const connectedCallback = element.prototype.connectedCallback || emptyFunction;
-    const disconnectedCallback = element.prototype.disconnectedCallback || emptyFunction;
+	const emptyFunction = () => { };
+	const connectedCallback = element.prototype.connectedCallback || emptyFunction;
+	const disconnectedCallback = element.prototype.disconnectedCallback || emptyFunction;
 
-    element.prototype.attributeChangedCallback = function (name: string, oldValue, newValue) {
-        if (newValue != oldValue) {
-            this[name] = convertStringToDataType(newValue);
-        }
-    };
+	element.prototype.attributeChangedCallback = function (name: string, oldValue, newValue) {
+		if (newValue != oldValue) {
+			this[name] = convertStringToDataType(newValue);
+		}
+	};
 
-    element.prototype.lsStatic = initLsStatic(element.prototype.lsStatic);
+	element.prototype.lsStatic = initLsStatic(element.prototype.lsStatic);
 
-    Object.defineProperty(element.prototype.constructor, 'observedAttributes', createGetterAndSetterForObservedAttributes(element.prototype.lsStatic));
+	Object.defineProperty(element.prototype.constructor, 'observedAttributes', createGetterAndSetterForObservedAttributes(element.prototype.lsStatic));
 
-    element.prototype.connectedCallback = function () {
-        const self: LSCustomElement = this;
-        if (!self.ls?.alreadyConnected) {
-            self.ls = {};
-            addEventDispatchers(self);
-            addElementsReferences(self);
-            addProperties(self);
-            addAttributes(self);
-            addReduxStores(self);
-            executeFirstRender(self);
-            self.ls.alreadyConnected = true;
+	element.prototype.connectedCallback = function () {
+		const self: LSCustomElement = this;
+		if (!self.ls?.alreadyConnected) {
+			self.ls = {};
+			addEventDispatchers(self);
+			addElementsReferences(self);
+			addProperties(self);
+			addAttributes(self);
+			addReduxStores(self);
+			executeFirstRender(self);
+			self.ls.alreadyConnected = true;
 
-            //Lifecycle methods
-            if (self.componentWillMount) {
-                self.componentWillMount();
-            }
-            connectedCallback.call(self);
-            if (self.componentDidMount) {
-                self.componentDidMount();
-            }
-        }
-    };
+			//Lifecycle methods
+			if (self.componentWillMount) {
+				self.componentWillMount();
+			}
+			connectedCallback.call(self);
+			if (self.componentDidMount) {
+				self.componentDidMount();
+			}
+		}
+	};
 
-    element.prototype.disconnectedCallback = function () {
-        if (this.componentWillUnmount) {
-            this.componentWillUnmount();
-        }
-        disconnectedCallback.call(this);
-        if (this.componentDidUnmount) {
-            this.componentDidUnmount();
-        }
-    };
+	element.prototype.disconnectedCallback = function () {
+		if (this.componentWillUnmount) {
+			this.componentWillUnmount();
+		}
+		disconnectedCallback.call(this);
+		if (this.componentDidUnmount) {
+			this.componentDidUnmount();
+		}
+	};
 
-    window.customElements.define(tag, element, { extends: config.extends });
+	window.customElements.define(tag, element, { extends: config.extends });
 };
