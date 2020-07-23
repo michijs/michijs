a21d6e331089e30d827656f4b3b9ca4d26ffa315
Lucas Segurado <segurado1996@gmail.com>
2020-07-23 15:14:51 -0300

    Linter commit

--- src/LSElement/classes/IdGenerator.ts
+++ src/LSElement/classes/IdGenerator.ts
@@ -4,18 +4,18 @@ export class IdGenerator {
     private ids: Map<string, string>;
 
     constructor() {
-        this.ids = new Map<string, string>();
+    	this.ids = new Map<string, string>();
     }
 
     public get(key: string) {
-        let returnId = '';
-        if (this.ids.has(key)) {
-            returnId = this.ids.get(key);
-        } else {
-            const newId = v4();
-            this.ids.set(key, newId);
-            returnId = newId;
-        }
-        return { id: returnId };
+    	let returnId = '';
+    	if (this.ids.has(key)) {
+    		returnId = this.ids.get(key);
+    	} else {
+    		const newId = v4();
+    		this.ids.set(key, newId);
+    		returnId = newId;
+    	}
+    	return { id: returnId };
     }
 }
