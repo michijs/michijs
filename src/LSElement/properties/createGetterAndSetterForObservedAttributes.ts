36c3991832e01a0afa5cd866df286442c0fa8d27
Lucas Segurado <segurado1996@gmail.com>
2020-08-12 17:52:25 -0300

    Fix for listen all attributes changes

--- src/LSElement/properties/createGetterAndSetterForObservedAttributes.ts
+++ src/LSElement/properties/createGetterAndSetterForObservedAttributes.ts
@@ -2,5 +2,5 @@ import { LsStaticAttributesType } from '../types';
 import { standardizePropertyName } from './standardizePropertyName';
 
 export function getObservedAttributes(lsStatic: LsStaticAttributesType) {
-	return lsStatic.observedAttributes.filter(attribute => attribute.options?.reflect).map(attribute => standardizePropertyName(attribute.propertyName));
+	return lsStatic.observedAttributes.map(attribute => standardizePropertyName(attribute.propertyName));
 }
