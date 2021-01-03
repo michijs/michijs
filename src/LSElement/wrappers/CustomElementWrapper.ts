b1ed9d6aaaa5abf2fdfe1e75d064b7603111e0fd
Lucas Segurado <segurado1996@gmail.com>
2021-01-03 17:02:01 -0300

    Minor type fix

--- src/LSElement/wrappers/CustomElementWrapper.ts
+++ src/LSElement/wrappers/CustomElementWrapper.ts
@@ -1,8 +1,8 @@
 import { h } from '../h';
-import { LSCustomElement } from '../types';
+import { LSCustomElement, RenderResult } from '../types';
 
 export function CustomElementWrapper<T>(component: {prototype: LSCustomElement}) {
-  return function (attrs: T, children) {
+  return function (attrs: T, children): RenderResult {
     const lsStatic = component.prototype.lsStatic;
     if (lsStatic.extends) {
       return h.createElement(lsStatic.extends, { ...attrs, is: lsStatic.tag }, ...children);
