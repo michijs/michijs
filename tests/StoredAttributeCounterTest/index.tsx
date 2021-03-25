22dadd7b27aa580da8ada0baa5eb958cabc23c8a
Lucas Segurado <segurado1996@gmail.com>
2021-03-25 10:27:13 -0300

    Lint commit

--- tests/StoredAttributeCounterTest/index.tsx
+++ tests/StoredAttributeCounterTest/index.tsx
@@ -2,34 +2,34 @@ import { AutonomousCustomElement, h, AdoptedStyle, EventDispatcher, CustomEventD
 import css from '../Shared/Counter.css';
 import { MyCounterAttributes } from '../Types';
 
-  @AutonomousCustomElement()
+@AutonomousCustomElement()
 export class StoredAttributeCounter extends HTMLElement implements LSCustomElement {
-      @StoredAttribute({ reflect: true, key: 'count', method: 'localStorage' }) count = 0;
-      @EventDispatcher() countChanged: CustomEventDispatcher<number>;
+  @StoredAttribute({ reflect: true, key: 'count', method: 'localStorage' }) count = 0;
+  @EventDispatcher() countChanged: CustomEventDispatcher<number>;
 
-      @Observer('count')
-      onCountChanged(newValue: number, _oldValue: number) {
-        this.countChanged.dispatch(newValue);
-      }
+  @Observer('count')
+  onCountChanged(newValue: number, _oldValue: number) {
+    this.countChanged.dispatch(newValue);
+  }
 
-      decrementCount(){
-        this.count--;
-      }
+  decrementCount() {
+    this.count--;
+  }
 
-      incrementCount(){
-        this.count++;
-      }
+  incrementCount() {
+    this.count++;
+  }
 
-      render() {
-        return (
-          <>
-            <AdoptedStyle parentRef={this} id="style">{css}</AdoptedStyle>
-            <button id="decrement-count" onpointerup={this.decrementCount}>-</button>
-            <span id='count'>{this.count}</span>
-            <button id="increment-count" onpointerup={this.incrementCount}>+</button>
-          </>
-        );
-      }
+  render() {
+    return (
+      <>
+        <AdoptedStyle parentRef={this} id="style">{css}</AdoptedStyle>
+        <button id="decrement-count" onpointerup={this.decrementCount}>-</button>
+        <span id='count'>{this.count}</span>
+        <button id="increment-count" onpointerup={this.incrementCount}>+</button>
+      </>
+    );
+  }
 }
 
 export default CustomElementWrapper<MyCounterAttributes>()(StoredAttributeCounter);
