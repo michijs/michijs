90f1abc6f1d05b4ac78c1431db7dc354af9e52f4
Lucas Segurado <segurado1996@gmail.com>
2021-03-25 10:26:34 -0300

    Renamed tests

--- tests/StoredAttributeCounterTest/index.tsx
+++ tests/StoredAttributeCounterTest/index.tsx
@@ -1,35 +1,35 @@
 import { AutonomousCustomElement, h, AdoptedStyle, EventDispatcher, CustomEventDispatcher, LSCustomElement, CustomElementWrapper, Observer, StoredAttribute } from '../../src';
 import css from '../Shared/Counter.css';
-import { MyCounterAttributes } from "../Types";
+import { MyCounterAttributes } from '../Types';
 
-@AutonomousCustomElement()
+  @AutonomousCustomElement()
 export class StoredAttributeCounter extends HTMLElement implements LSCustomElement {
-    @StoredAttribute({ reflect: true, key: 'count', method: 'localStorage' }) count = 0;
-    @EventDispatcher() countChanged: CustomEventDispatcher<number>;
+      @StoredAttribute({ reflect: true, key: 'count', method: 'localStorage' }) count = 0;
+      @EventDispatcher() countChanged: CustomEventDispatcher<number>;
 
-    @Observer('count')
-    onCountChanged(newValue: number, _oldValue: number) {
-      this.countChanged.dispatch(newValue);
-    }
+      @Observer('count')
+      onCountChanged(newValue: number, _oldValue: number) {
+        this.countChanged.dispatch(newValue);
+      }
 
-    decrementCount(){
-      this.count--;
-    }
+      decrementCount(){
+        this.count--;
+      }
 
-    incrementCount(){
-      this.count++;
-    }
+      incrementCount(){
+        this.count++;
+      }
 
-    render() {
-      return (
-        <>
-          <AdoptedStyle parentRef={this} id="style">{css}</AdoptedStyle>
-          <button id="decrement-count" onpointerup={this.decrementCount}>-</button>
-          <span id='count'>{this.count}</span>
-          <button id="increment-count" onpointerup={this.incrementCount}>+</button>
-        </>
-      );
-    }
+      render() {
+        return (
+          <>
+            <AdoptedStyle parentRef={this} id="style">{css}</AdoptedStyle>
+            <button id="decrement-count" onpointerup={this.decrementCount}>-</button>
+            <span id='count'>{this.count}</span>
+            <button id="increment-count" onpointerup={this.incrementCount}>+</button>
+          </>
+        );
+      }
 }
 
 export default CustomElementWrapper<MyCounterAttributes>()(StoredAttributeCounter);
