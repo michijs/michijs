bf1ccef712ae7e5ecdce02ff4bec8e0f7906237d
Lucas Segurado <segurado1996@gmail.com>
2021-11-15 19:40:28 -0300

    Updated render Functional Component

--- src/LSElement/DOM/renderFunctionalComponent.ts
+++ src/LSElement/DOM/renderFunctionalComponent.ts
@@ -1,9 +1,5 @@
 import { LSNode } from '../experiments/LSNode/LSNode';
 
 export function renderFunctionalComponent(jsxElement: JSX.Element) {
-  const fragment = document.createDocumentFragment();
-  if (jsxElement) {
-    fragment.append(LSNode(jsxElement).el);
-  }
-  return fragment;
+  return LSNode(jsxElement).valueOf();
 }
