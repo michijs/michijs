00b88502326eb50ab27c5d5fd9617b2a05645739
Lucas Segurado <segurado1996@gmail.com>
2021-12-08 14:36:24 -0300

    2.1.9

--- src/LSElement/LSNode/LSFunctionNode.ts
+++ src/LSElement/LSNode/LSFunctionNode.ts
@@ -48,7 +48,6 @@ export class LSFunctionNode implements LSChildNode<FunctionJSXElement> {
     if (type === JSXElementType.FUNCTION && typedNewJSXElement<FunctionJSXElement>().tag === this.jsxElement.tag) {
       const newFunctionResult = toNonFunctionJSXElement(typedNewJSXElement<FunctionJSXElement>(), this.self);
       this.node = this.node.updateElement(newFunctionResult);
-      this.jsxElement.attrs = typedNewJSXElement<FunctionJSXElement>().attrs;
       return this;
     }
     return this.node.replaceWith(newJSXElement, this.isSVG);
