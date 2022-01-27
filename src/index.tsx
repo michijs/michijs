37d668af8764af39ad162386b92c41945d7e0022
Lucas Segurado <segurado1996@gmail.com>
2022-01-27 14:15:46 -0300

    Minor bugfix

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ export * from './LSElement/types';
 export * from './LSElement/DOM';
 export * from './LSElement/i18n';
 export * from '@lsegurado/htmltype';
-import './LSElement/h/global';
 import './LSElement/h/JSX';
 export { createCustomElement } from './LSElement/customElements/createCustomElement';
 export { createStyleSheet, css } from './LSElement/css/createStyleSheet';
