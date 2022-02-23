6fc6916630623e2d460ceae8562e263404fe9d35
Lucas Segurado <segurado1996@gmail.com>
2022-02-23 11:01:51 -0300

    Updated some deprecated functions

--- src/LSElement/DOM/attributes/setAttributeOrProperty.ts
+++ src/LSElement/DOM/attributes/setAttributeOrProperty.ts
@@ -2,7 +2,7 @@ import { setAttribute } from './setAttribute';
 
 export function setAttributeOrProperty(element: Element | HTMLElement, name: string, newValue: any) {
   if (name.startsWith('_')) {
-    const propertyName = name.substr(1);
+    const propertyName = name.substring(1);
     element[propertyName] = newValue;
   } else {
     setAttribute(element, name, newValue);
--- src/LSElement/utils/formatToKebabCase.ts
+++ src/LSElement/utils/formatToKebabCase.ts
@@ -3,7 +3,7 @@ import { KebabCase } from '../types';
 export function formatToKebabCase<T extends string>(variable: T): KebabCase<T> {
   let formattedVariable = variable.replace(/[A-Z]/g, m => `-${m.toLowerCase()}`);
   if (formattedVariable.startsWith('-')) {
-    formattedVariable = formattedVariable.substr(1, formattedVariable.length);
+    formattedVariable = formattedVariable.substring(1, formattedVariable.length);
   }
   return formattedVariable as KebabCase<T>;
 }
