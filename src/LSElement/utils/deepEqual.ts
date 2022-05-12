c27ccf42eeffd1959a76413e9054344dde890cf9
Lucas Segurado <segurado1996@gmail.com>
2022-05-12 19:28:35 -0300

    Bugfixes

--- src/LSElement/utils/deepEqual.ts
+++ src/LSElement/utils/deepEqual.ts
@@ -1,9 +1,12 @@
 export function deepEqual(object1, object2) {
+  if (object1 === object2)
+    return true;
+  
   const type = typeof object1;
   const areDifferentTypes = type !== typeof object2;
-  if (areDifferentTypes) {
+  if (areDifferentTypes)
     return false;
-  }
+
   switch (type) {
     case 'function': {
       return object1.name === object2.name && object1.toString() === object2.toString();
@@ -13,20 +16,19 @@ export function deepEqual(object1, object2) {
       if (Array.isArray(object1)) {
         if (object1.length !== object2.length)
           return false;
-
         return !object1.find((value, index) => !deepEqual(value, object2[index]));
       }
       // null can't have keys, so I use the non-null value
-      for (const key in (object1 ?? object2)) {
-        if (!deepEqual(object1?.[key], object2?.[key]))
+      for (const key in (object1 !== null && object1 !== void 0 ? object1 : object2)) {
+        if (!deepEqual(object1 === null || object1 === void 0 ? void 0 : object1[key], object2 === null || object2 === void 0 ? void 0 : object2[key]))
           return false;
       }
       return true;
     }
-    default: return object1 === object2;
   }
+  return false;
+  
 }
-
 // export function deepEqual(object1, object2) {
 //   const result = deepEqual2(object1, object2)
 //   console.log(object1, object2, result)
