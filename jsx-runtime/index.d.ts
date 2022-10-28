27bc9f2b3da020e2b0a279ab9f84e9f82782b3a8
Lucas Segurado <lsegurado1996@gmail.com>
2022-10-28 22:28:05 +0200

    Added jsx runtime type definitions and minor bugfix

---
+++ jsx-runtime/index.d.ts
@@ -0,0 +1,8 @@
+import { h as oldH, Fragment } from '../dist'
+
+export declare const h: {
+    jsx: typeof oldH.createElement,
+    jsxs: typeof oldH.createElement,
+    jsxDEV: typeof oldH.createElement,
+    Fragment: typeof Fragment
+};
