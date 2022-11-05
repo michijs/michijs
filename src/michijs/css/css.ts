223afbfb1ca193ef9104f7301ec8d5c11c1b0a10
Lucas Segurado <lsegurado1996@gmail.com>
2022-11-05 23:52:36 +0100

    Bugfix

--- src/michijs/css/css.ts
+++ src/michijs/css/css.ts
@@ -4,13 +4,14 @@
  * Recomended extension for VSCode: 
  * @link https://marketplace.visualstudio.com/items?itemName=paulmolluzzo.convert-css-in-js
  */
- export const css = (cssObject: TemplateStringsArray, ...props: (string | number)[]) => {
-    const styleSheet = new CSSStyleSheet();
-    styleSheet.replaceSync(cssObject.raw.reduce((previousValue, currentValue, i) => {
-      const type = typeof props[i];
-      if (type === 'string' || type === 'number')
-        return `${previousValue}${currentValue}${props[i]}`;
-      return `${previousValue}${currentValue}`;
-    }));
-    return styleSheet;
-  };
+export const css = (cssObject: TemplateStringsArray, ...props: (string | number)[]) => {
+  const styleSheet = new CSSStyleSheet();
+  styleSheet.replaceSync(cssObject.raw.reduce((previousValue, currentValue, i) => {
+    const type = typeof props[i];
+    if (type === 'string' || type === 'number')
+      return `${previousValue}${currentValue}${props[i]}`;
+    return `${previousValue}${currentValue}`;
+    // The accumulator takes the first value if you don't pass a value as the second argument:
+  }, ''));
+  return styleSheet;
+};
