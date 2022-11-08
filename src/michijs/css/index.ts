20f35609e6752a77fdb50819394f0e4c98374bd3
Lucas Segurado <lsegurado1996@gmail.com>
2022-11-08 20:24:14 +0100

    Added aux function for computedStyleSheet

--- src/michijs/css/index.ts
+++ src/michijs/css/index.ts
@@ -1,5 +1,6 @@
 export { createStyleSheet } from './createStyleSheet';
 export { updateStyleSheet } from './updateStyleSheet';
+export { createComputedStyleSheet } from './createComputedStyleSheet';
 export { css } from './css';
 export { ruleListFromCssObject } from './ruleListFromCssObject';
 export { supportsAdoptingStyleSheets } from './supportsAdoptingStyleSheets';
