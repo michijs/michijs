bb95f45c7b0e06dcf103172e6d061e8e570c95ee
Lucas Segurado <lsegurado1996@gmail.com>
2022-11-09 20:37:42 +0100

    Built-in elements bugfix

--- src/michijs/DOMDiff/ObjectFactory.ts
+++ src/michijs/DOMDiff/ObjectFactory.ts
@@ -13,13 +13,17 @@ export const ObjectFactory: ElementFactory = {
     const { children, $staticChildren, $oncreated, $doNotTouchChildren, $onupdate, ...attrs } = jsx.attrs;
     if (isSVG) {
       if (jsx.attrs?.is) {
-        el = document.createElementNS('http://www.w3.org/2000/svg', jsx.tag, jsx.attrs.is);
+        el = document.createElementNS('http://www.w3.org/2000/svg', jsx.tag, {
+          is: jsx.attrs.is
+        });
       } else {
         el = document.createElementNS('http://www.w3.org/2000/svg', jsx.tag);
       }
     }
     else if (jsx.attrs?.is)
-      el = document.createElement(jsx.tag, jsx.attrs.is);
+      el = document.createElement(jsx.tag, {
+        is: jsx.attrs.is
+      });
     else 
       el = document.createElement(jsx.tag);
     
