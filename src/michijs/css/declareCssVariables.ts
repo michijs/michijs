8963203974d5821547428d50473af8f31369f070
Lucas Segurado <lsegurado1996@gmail.com>
2022-11-14 20:39:31 +0100

    Added declare css variables utility

--- src/michijs/css/declareCssVariables.ts
+++ src/michijs/css/declareCssVariables.ts
@@ -1,15 +1,16 @@
 import { createStyleSheet } from ".";
 import { CSSObject, StringObjectOf } from "../types";
 import { valueIsCSSObject } from "../typeWards/valueIsCSSObject";
+import { formatToKebabCase } from "../utils";
 import { cssVariablesFromCssObject } from "./cssVariablesFromCssObject";
 
 const fakeCSSObject = (parent: string, target: CSSObject) => new Proxy(target, {
   get(t, p: string) {
     const property = t[p];
     if (valueIsCSSObject(property))
-      return fakeCSSObject(`${parent}-${p.toString()}`, property)
+      return fakeCSSObject(`${parent}-${formatToKebabCase(p.toString())}`, property)
     else
-      return `var(${parent}-${p.toString()})`
+      return `var(${parent}-${formatToKebabCase(p.toString())})`
   }
 })
 
