28bd3db4afebdded5dd24e7c50351fcf89ebe418
Lucas Segurado <lsegurado1996@gmail.com>
2022-11-16 22:54:58 +0100

    Improved CSS objects

--- src/michijs/css/declareCssVariables.ts
+++ src/michijs/css/declareCssVariables.ts
@@ -1,23 +1,11 @@
 import { createStyleSheet } from ".";
-import { CSSObject, StringObjectOf } from "../types";
-import { valueIsCSSObject } from "../typeWards/valueIsCSSObject";
-import { formatToKebabCase } from "../utils";
+import { CSSObject } from "../types";
 import { cssVariablesFromCssObject } from "./cssVariablesFromCssObject";
 
-const fakeCSSObject = (parent: string, target: CSSObject) => new Proxy(target, {
-  get(t, p: string) {
-    const property = t[p];
-    if (valueIsCSSObject(property))
-      return fakeCSSObject(`${parent}-${formatToKebabCase(p.toString())}`, property)
-    else
-      return `var(${parent}-${formatToKebabCase(p.toString())})`
-  }
-})
-
-export function declareCssVariables<T extends CSSObject>(selector: string, cssObject: T): [CSSStyleSheet, StringObjectOf<T>] {
+export function declareCssVariables<T extends CSSObject>(selector: string, cssObject: T): CSSStyleSheet {
   const styleSheet = createStyleSheet({
     [selector]: cssVariablesFromCssObject(cssObject)
   });
 
-  return [styleSheet, fakeCSSObject('-', cssObject)]
+  return styleSheet
 }
---
+++ src/michijs/css/declareCssVariablesRef.ts
@@ -0,0 +1,14 @@
+import { CSSObject, StringObjectOf } from "../types";
+import { formatToKebabCase } from "../utils";
+
+export function declareCssVariablesRef<T extends CSSObject>(parent = '-') {
+  return new Proxy({}, {
+    get(_, p) {
+      if (Symbol.toPrimitive === p)
+        return () => `var(${parent})`;
+
+      else
+        return declareCssVariablesRef(`${parent}-${formatToKebabCase(p.toString())}`);
+    }
+  }) as StringObjectOf<T>;
+}
