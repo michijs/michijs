202c61a62049e7a8c4239f343ee48581f4d147ba
Lucas Segurado <lsegurado1996@gmail.com>
2022-12-21 17:24:02 +0100

    Css variables bugfixes

--- src/michijs/DOM/attributes/setStyleProperty.ts
+++ src/michijs/DOM/attributes/setStyleProperty.ts
@@ -1,7 +1,7 @@
 import { formatToKebabCase } from "../../utils"
 
 export const setStyleProperty = (element: HTMLElement, key: string, value: unknown) => {
-  if (value !== undefined)
+  if (value !== undefined && value !== null)
     element.style.setProperty(formatToKebabCase(key), value.toString())
   else
     element.style.removeProperty(formatToKebabCase(key))
--- src/michijs/css/createCssVariables.ts
+++ src/michijs/css/createCssVariables.ts
@@ -2,10 +2,6 @@ import { createStyleSheet } from ".";
 import { CSSObject } from "../types";
 import { cssVariablesFromCssObject } from "./cssVariablesFromCssObject";
 
-export function createCssVariables<T extends CSSObject>(selector: string, cssObject: T): CSSStyleSheet {
-  const styleSheet = createStyleSheet({
-    [selector]: cssVariablesFromCssObject(cssObject)
-  });
-
-  return styleSheet
+export function createCssVariables<T extends CSSObject>(selector: string | string[], cssObject: T): CSSStyleSheet {
+  return createStyleSheet(cssVariablesFromCssObject(cssObject), typeof selector === 'string' ? [selector]: selector);
 }
