dc95c1c44ecb8f266086eb705529a6ec5105ebd3
Lucas Segurado <lsegurado1996@gmail.com>
2023-01-18 12:38:08 +0100

    Improved routing and added I18n docs

--- src/michijs/routing/createRouter.tsx
+++ src/michijs/routing/createRouter.tsx
@@ -1,7 +1,7 @@
 import { Router } from '../components/Router';
 import { h } from '../h';
 import { formatToKebabCase } from '../utils/formatToKebabCase';
-import { PagesFunction, CreateRouterResult, UrlFunction, CreateRouterSearchParamsAndHash, Route } from './types';
+import { PagesFunction, CreateRouterResult, UrlFunction, Route } from './types';
 import { setSearchParam } from './utils/setSearchParam';
 
 export const urlFn = (property: string, parentRoute?: UrlFunction<any, any>): UrlFunction<any, any> => {
@@ -19,25 +19,23 @@ export const urlFn = (property: string, parentRoute?: UrlFunction<any, any>): Ur
   };
 };
 
-export function createRouter<T extends CreateRouterSearchParamsAndHash>() {
-  return function <R extends Record<string, Route>>(routes: R, parentRoute?: UrlFunction<any, any>) {
-    const urls = new Proxy({}, {
-      get(_, property: string) {
-        return urlFn(property, parentRoute);
-      }
-    });
-    const pages = new Proxy({}, {
-      get(_, _property) {
-        return (fn: PagesFunction<any, any>) => {
-          return fn;
-        };
-      }
-    });
+export function createRouter<R extends Record<string, Route>>(routes: R, parentRoute?: UrlFunction<any, any>) {
+  const urls = new Proxy({}, {
+    get(_, property: string) {
+      return urlFn(property, parentRoute);
+    }
+  });
+  const pages = new Proxy({}, {
+    get(_, _property) {
+      return (fn: PagesFunction<any, any>) => {
+        return fn;
+      };
+    }
+  });
 
-    const RouterProxy: CreateRouterResult<T, R>['Router'] = (props) => {
-      return <Router {...props} _routes={routes} _parentRoute={parentRoute} />;
-    };
+  const RouterProxy: CreateRouterResult<R>['Router'] = (props) => {
+    return <Router {...props} _routes={routes} _parentRoute={parentRoute} />;
+  };
 
-    return { urls, Router: RouterProxy, pages } as CreateRouterResult<T, R>;
-  }
+  return { urls, Router: RouterProxy, pages } as CreateRouterResult<R>;
 }
