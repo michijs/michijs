fa6c8af21bbba66e89ec515a2012282ad52aebae
Lucas Segurado <lsegurado1996@gmail.com>
2023-02-01 18:35:12 +0100

    Excluded unit tests and fixed jsx-runtime

--- jsx-runtime/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { h as oldH, Fragment } from '../dist'
-
-export declare const h: {
-    jsx: typeof oldH.createElement,
-    jsxs: typeof oldH.createElement,
-    jsxDEV: typeof oldH.createElement,
-    Fragment: typeof Fragment
-};
