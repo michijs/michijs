34ee29b55505c726eb5bb3be9c217d28b2e338cf
Lucas Segurado <lsegurado1996@gmail.com>
2023-02-11 02:43:22 +0100

    Fix for observable value

--- src/michijs/hooks/observableValue.ts
+++ src/michijs/hooks/observableValue.ts
@@ -1,9 +1,10 @@
+import { ObservableLike } from '../types';
 import { observable } from './observable';
 import { observe } from './observe';
 
 export function observableValue<T = unknown>(initialValue: T) {
   const { notify, ...observableProps } = observable<string>();
-  const state = observe<{ value: T }>({
+  const state = observe<{ value: T } & ObservableLike<string>>({
     item: { value: initialValue, ...observableProps },
     onChange: (propertyPath) => {
       if (propertyPath)
