5b7b2291b0321188f3f14f7591be92306d4ce815
Lucas Segurado <lsegurado1996@gmail.com>
2023-12-04 18:03:13 +0100

    Small bugfix

--- src/michijs/utils/formatToKebabCase.ts
+++ src/michijs/utils/formatToKebabCase.ts
@@ -3,6 +3,6 @@ import { KebabCase } from "../types";
 export function formatToKebabCase<T extends string>(variable: T): KebabCase<T> {
   return variable.replace(
     /[A-Z]/g,
-    (m) => `-${m.toLowerCase()}`,
+    (m, index) => index === 0 ? m.toLowerCase() : `-${m.toLowerCase()}`,
   ) as KebabCase<T>;
 }
