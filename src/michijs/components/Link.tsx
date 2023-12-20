a6a0d7889289d4b7d45fe4d81e8b531760ca2980
Lucas Segurado <lsegurado1996@gmail.com>
2023-12-20 20:39:04 +0100

    Added suggested change on github issue

--- src/michijs/components/Link.tsx
+++ src/michijs/components/Link.tsx
@@ -5,7 +5,7 @@ import { goTo } from "../routing";
 type LinkProps = {
   state?: any;
   title?: string;
-  url: URL | string;
+  url: URL | string | (() => URL | string);
 } & JSX.IntrinsicElements["a"];
 
 /**Provides the ability to move around the web page without reloading the page. It uses the same attributes as an anchor tag but also allows the use of URL objects. Uses the goTo method. */
@@ -13,7 +13,8 @@ export const Link: FC<LinkProps> = (
   { state = {}, title = document.title, url, ...attrs },
   self,
 ) => {
-  const href = typeof url === "string" ? url : url.href;
+  const finalUrl = typeof url === 'function' ? url(): url;
+  const href = typeof finalUrl === "string" ? finalUrl : finalUrl.href;
   return (
     <a
       {...attrs}
@@ -21,7 +22,7 @@ export const Link: FC<LinkProps> = (
       onclick={(e) => {
         try {
           e.preventDefault();
-          goTo(url);
+          goTo(finalUrl);
           attrs.onclick?.apply(self, [e]);
         } catch {
           window.location.href = href;
