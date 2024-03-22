0a688149a9116843ddf714a2894ca83b668a98e6
Lucas Segurado <lsegurado1996@gmail.com>
2024-03-22 21:31:52 +0100

    Tests

---
+++ src/michijs/components/Slot.ts
@@ -0,0 +1,18 @@
+import type { HTMLElements } from "../generated/htmlType";
+import { isMichiCustomElement } from "../typeWards/isMichiCustomElement";
+import { FC } from "../types";
+
+type SlotProps = HTMLElements["slot"];
+
+/**Allows to set attributes and event listeners to the host element itself. */
+export const Slot: FC<SlotProps> = ({ children, ...attrs }, options) => {
+  const self = options?.contextElement;
+  if (self && isMichiCustomElement(self))
+    if (attrs.name) {
+      // self.slot 
+    } else {
+
+    }
+
+  return null;
+};
