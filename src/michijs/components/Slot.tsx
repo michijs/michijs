306783eb58660fbbf5a3971bff78842ab17d0701
Lucas Martin Segurado <lsegurado1996@gmail.com>
2024-03-27 22:31:09 +0100

    Added slot

---
+++ src/michijs/components/Slot.tsx
@@ -0,0 +1,23 @@
+import type { HTMLElements } from "../generated/htmlType";
+import { isMichiCustomElement } from "../typeWards/isMichiCustomElement";
+import { FC } from "../types";
+import { getShadowRoot } from "../utils/getShadowRoot";
+
+type SlotProps = HTMLElements["slot"];
+
+/**Allows to set attributes and event listeners to the host element itself. */
+export const Slot: FC<SlotProps> = (attrs, options) => {
+  const self = options?.contextElement;
+  if (self && isMichiCustomElement(self)){
+    if(getShadowRoot(self))
+      return <slot {...attrs}/>
+    else {
+      if (attrs.name) {
+        // self.slot
+      } else {
+      }
+    }
+  }
+
+  return null;
+};
