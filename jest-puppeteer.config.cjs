7414328cb314f5bd71651a1d02af8c3a0a73fb6d
Lucas Martin Segurado <lsegurado1996@gmail.com>
2024-04-01 19:16:26 +0200

    Replaced npm with bun

--- jest-puppeteer.config.cjs
+++ jest-puppeteer.config.cjs
@@ -2,8 +2,8 @@
 const env = process.env.NODE_ENV.trimEnd();
 let command;
 
-if (env === "TESTING") command = "npm run start-benchmark-michijs";
-else command = "npm run start-benchmark-vanilla";
+if (env === "TESTING") command = "bun run start-benchmark-michijs";
+else command = "bun run start-benchmark-vanilla";
 
 module.exports = {
   launch: {
