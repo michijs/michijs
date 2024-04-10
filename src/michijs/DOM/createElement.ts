613ebc5348fb68400899669c2ff46c7be8ea0fb4
Lucas Martin Segurado <lsegurado1996@gmail.com>
2024-04-10 18:34:49 -0300

    Small change

--- src/michijs/DOM/createElement.ts
+++ src/michijs/DOM/createElement.ts
@@ -1,21 +1,28 @@
+import { createObject } from "../DOMDiff/createObject";
 import type { CreateOptions, GetElementProps } from "../types";
-import { setProperties } from "./attributes/setProperties";
 
 export function createElement<
   const T extends string,
   A extends GetElementProps<T> | JSX.IntrinsicElements["div"],
   E extends T extends keyof HTMLElementTagNameMap
-    ? HTMLElementTagNameMap[T]
-    : T extends keyof SVGElementTagNameMap
-      ? SVGElementTagNameMap[T]
-      : HTMLElement,
+  ? HTMLElementTagNameMap[T]
+  : T extends keyof SVGElementTagNameMap
+  ? SVGElementTagNameMap[T]
+  : HTMLElement,
 >(
   tagName: T,
   attributes?: A,
-  options?: ElementCreationOptions & CreateOptions,
+  { is, ...options }: ElementCreationOptions & CreateOptions = {},
 ) {
-  const el = document.createElement(tagName, options);
-  if (attributes) setProperties(el, attributes, options);
+
+  const el = createObject({
+    jsxTag: tagName,
+    attrs: {
+      ...(attributes ?? {}),
+      is
+    }
+  },
+  options)
 
   return el as unknown as E & ((props?: A) => E);
 }
