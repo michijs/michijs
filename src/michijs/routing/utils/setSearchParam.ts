c9038059b39c1cca14f918b4623f3cb98d439644
Lucas Martin Segurado <lsegurado1996@gmail.com>
2024-04-17 13:33:30 -0300

    setSearchParam bugfix for undefined and null values

--- src/michijs/routing/utils/setSearchParam.ts
+++ src/michijs/routing/utils/setSearchParam.ts
@@ -1,6 +1,10 @@
+import { isNil } from "../../utils";
+
 export const setSearchParam = (url: URL, name: string, value: unknown) => {
   const valueOf = value?.valueOf();
-  if (valueOf !== undefined)
+  if (isNil(valueOf))
+    url.searchParams.delete(name)
+  else
     url.searchParams.set(
       name,
       typeof valueOf === "object"
