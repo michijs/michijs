c41fe3c61c53db237469bf42efe9c313c962091d
Lucas Segurado <lsegurado1996@gmail.com>
2024-06-04 17:08:18 +0200

    Showing errors on console instead of silently failing

--- src/michijs/hooks/usePromise.ts
+++ src/michijs/hooks/usePromise.ts
@@ -46,6 +46,7 @@ export const usePromise = <R>(
             recall,
           };
         } catch (ex) {
+          console.error(ex)
           return {
             error: new Error(ex),
             loading: false,
