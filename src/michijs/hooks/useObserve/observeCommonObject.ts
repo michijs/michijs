0351ca0b3fa60e42c1a976a92535c531575d18c5
Lucas Segurado <lsegurado1996@gmail.com>
2024-06-09 23:07:37 +0200

    Small bugfix

--- src/michijs/hooks/useObserve/observeCommonObject.ts
+++ src/michijs/hooks/useObserve/observeCommonObject.ts
@@ -11,16 +11,20 @@ export const customObjectSet =
   <T>(
     initialObservers: Subscription<T>[],
   ): CommonObjectProxyHandler<any>["set"] =>
-  (target, property, newValue, receiver) => {
-    if (!["length", "name"].includes(property as string) && property in target)
-      return Reflect.set(target, property, newValue, receiver);
+    (target, property, newValue, receiver) => {
+      if (!["length", "name"].includes(property as string) && property in target)
+        return Reflect.set(target, property, newValue, receiver);
 
-    // Not sure if this is possible today
-    if (target.$value) {
-      const oldValue = target.$value[property];
-      if (oldValue) {
-        oldValue(newValue);
-        return true;
+      // Not sure if this is possible today
+      if (target.$value) {
+        const oldValue = target.$value[property];
+        if (oldValue) {
+          // If you call something.myfunctionname = other function it should not call the function
+          if ((typeof oldValue.$value === 'function'))
+            oldValue.$value = newValue
+          else
+            oldValue(newValue);
+          return true;
       } else {
         const newItem = useObserve(newValue, initialObservers);
         const result = Reflect.set(target.$value, property, newItem);
@@ -29,7 +33,7 @@ export const customObjectSet =
         return result;
       }
     }
-    return false;
+return false;
   };
 
 export const customObjectDelete: CommonObjectProxyHandler<any>["deleteProperty"] =
@@ -50,24 +54,24 @@ export const customObjectGet =
   <T extends ObservableType<any>>(
     initialObservers: Subscription<T>[],
   ): CommonObjectProxyHandler<any>["get"] =>
-  (target, p, receiver) => {
-    // Because function already has length
-    if (!["length", "name"].includes(p as string) && p in target) {
-      return Reflect.get(target, p, receiver);
-    } else if (target.$value) {
-      if (typeof target.$value === "object")
-        if (p in target.$value)
-          return Reflect.get(target.$value, p, target.$value);
-        else customObjectSet(initialObservers)(target, p, undefined, receiver);
-      else if (target.$value[p]) return target.$value[p];
-      // If a nested object is undefined
-    } else {
-      target.$value = {};
-      customObjectSet(initialObservers)(target, p, undefined, receiver);
-    }
+    (target, p, receiver) => {
+      // Because function already has length
+      if (!["length", "name"].includes(p as string) && p in target) {
+        return Reflect.get(target, p, receiver);
+      } else if (target.$value) {
+        if (typeof target.$value === "object")
+          if (p in target.$value)
+            return Reflect.get(target.$value, p, target.$value);
+          else customObjectSet(initialObservers)(target, p, undefined, receiver);
+        else if (target.$value[p]) return target.$value[p];
+        // If a nested object is undefined
+      } else {
+        target.$value = {};
+        customObjectSet(initialObservers)(target, p, undefined, receiver);
+      }
 
-    return target.$value[p];
-  };
+      return target.$value[p];
+    };
 
 const functionProps = ["arguments", "caller", "prototype"];
 
@@ -81,10 +85,10 @@ export const customObjectGetOwnPropertyDescriptor: CommonObjectProxyHandler<any>
     return functionProps.includes(prop as string)
       ? Reflect.getOwnPropertyDescriptor(target, prop)
       : {
-          ...Reflect.getOwnPropertyDescriptor(target, prop),
-          enumerable: true,
-          configurable: true,
-        };
+        ...Reflect.getOwnPropertyDescriptor(target, prop),
+        enumerable: true,
+        configurable: true,
+      };
   };
 export const customObjectHas: CommonObjectProxyHandler<any>["has"] = (
   target,
@@ -124,8 +128,8 @@ export function observeCommonObject<T>(
   const newObservable = new ProxiedValue<T>(
     item && Object.getPrototypeOf(item) === Object.prototype
       ? cloneCommonObject(item, (value) =>
-          useObserve<any>(value, newInitialObservers),
-        )
+        useObserve<any>(value, newInitialObservers),
+      )
       : item,
     initialObservers,
   );
