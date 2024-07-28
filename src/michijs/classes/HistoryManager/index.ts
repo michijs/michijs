f460da5395d1fc2b656b3b84b1e16233b3fecadc
Lucas Segurado <lsegurado1996@gmail.com>
2024-07-28 10:26:49 +0200

    Removed temporally dynamic import

--- src/michijs/classes/HistoryManager/index.ts
+++ src/michijs/classes/HistoryManager/index.ts
@@ -1,14 +1,19 @@
 import type { HistoryManagerType } from "../../types";
+import { ModernHistoryManager } from "./ModernHistoryManager";
+import { LegacyHistoryManager } from "./LegacyHistoryManager";
 
 let HistoryManager: HistoryManagerType;
+// Rollup has issues with this
 if (window.navigation && window.URLPattern) {
-  HistoryManager = new (
-    await import("./ModernHistoryManager")
-  ).ModernHistoryManager();
+  HistoryManager = new ModernHistoryManager();
+  // HistoryManager = new (
+  //   await import("./ModernHistoryManager")
+  // ).ModernHistoryManager();
 } else {
-  HistoryManager = new (
-    await import("./LegacyHistoryManager")
-  ).LegacyHistoryManager();
+  HistoryManager = new LegacyHistoryManager();
+  // HistoryManager = new (
+  //   await import("./LegacyHistoryManager")
+  // ).LegacyHistoryManager();
 }
 
 export { HistoryManager };
