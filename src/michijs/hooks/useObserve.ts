a8eea70ecb6629ad3cfb52a8a40d3e44d14ff7e3
Lucas Segurado <lsegurado1996@gmail.com>
2024-08-03 01:45:21 +0200

    Small bugfix

--- src/michijs/hooks/useObserve.ts
+++ src/michijs/hooks/useObserve.ts
@@ -7,7 +7,7 @@ import { observeSet } from "./useObserve/observeSet";
 
 /**
  * Responsible for observing changes on different types of values.
- * @param item The value to be observed.
+ * @param itemValue The value to be observed.
  * @param initialObservers An array of initial observers of type Subscription<T>.
  * @returns A new observable
  */
@@ -15,35 +15,36 @@ export function useObserve<T>(
   item?: T,
   initialObservers?: Subscription<T>[],
 ): ObservableType<T> {
-  if (item) {
-    const typeofItem = typeof item;
+  const itemValue = item?.valueOf() as T
+  if (itemValue) {
+    const typeofItem = typeof itemValue;
     if (typeofItem === "object") {
-      if (Array.isArray(item))
-        return observeArray(item, initialObservers) as ObservableType<T>;
+      if (Array.isArray(itemValue))
+        return observeArray(itemValue, initialObservers) as ObservableType<T>;
       // Many built-in objects, for example Map, Set, Date, Promise and others make use of so-called internal slots.
       // These are like properties but reserved for internal, specification-only purposes.
       // For instance, Map stores items in the internal slot [[MapData]].
       // Built-in methods access them directly, not via [[Get]]/[[Set]] internal methods. So Proxy can’t intercept that.
-      if (item instanceof Date)
+      if (itemValue instanceof Date)
         return observeDate(
-          item,
+          itemValue,
           initialObservers,
         ) as unknown as ObservableType<T>;
-      if (item instanceof Map)
+      if (itemValue instanceof Map)
         return observeMap(
-          item,
+          itemValue,
           initialObservers,
         ) as unknown as ObservableType<T>;
-      if (item instanceof Set)
+      if (itemValue instanceof Set)
         return observeSet(
-          item,
+          itemValue,
           initialObservers,
         ) as unknown as ObservableType<T>;
       // console.error(`The object with path "${props.propertyPath}" cannot be observed ${item}`)
     }
   }
   return observeCommonObject<T>(
-    item as T,
+    itemValue as T,
     initialObservers,
   ) as unknown as ObservableType<T>;
 }
