db8a74dfcacf9ab2deb57201e7f6d42b590e75bf
Lucas Martin Segurado <lsegurado1996@gmail.com>
2024-08-11 13:46:33 +0200

    Update index.ts

--- src/michijs/classes/HistoryManager/index.ts
+++ src/michijs/classes/HistoryManager/index.ts
@@ -3,17 +3,14 @@ import { ModernHistoryManager } from "./ModernHistoryManager";
 import { LegacyHistoryManager } from "./LegacyHistoryManager";
 
 let HistoryManager: HistoryManagerType;
-// Rollup has issues with this
 if (window.navigation && window.URLPattern) {
-  HistoryManager = new ModernHistoryManager();
-  // HistoryManager = new (
-  //   await import("./ModernHistoryManager")
-  // ).ModernHistoryManager();
+  HistoryManager = new (
+    await import("./ModernHistoryManager")
+  ).ModernHistoryManager();
 } else {
-  HistoryManager = new LegacyHistoryManager();
-  // HistoryManager = new (
-  //   await import("./LegacyHistoryManager")
-  // ).LegacyHistoryManager();
+  HistoryManager = new (
+    await import("./LegacyHistoryManager")
+  ).LegacyHistoryManager();
 }
 
 export { HistoryManager };
