53129c57c705e37fb0740af34b3ca42b15579c91
Lucas Martin Segurado <lsegurado1996@gmail.com>
2024-08-11 13:46:58 +0200

    Update index.ts

--- src/michijs/classes/HistoryManager/index.ts
+++ src/michijs/classes/HistoryManager/index.ts
@@ -1,14 +1,14 @@
 import type { HistoryManagerType } from "../../types";
 
 let HistoryManager: HistoryManagerType;
-if (window.navigation && window.URLPattern) {
+if (window.navigation && window.URLPattern)
   HistoryManager = new (
     await import("./ModernHistoryManager")
   ).ModernHistoryManager();
-} else {
+else
   HistoryManager = new (
     await import("./LegacyHistoryManager")
   ).LegacyHistoryManager();
-}
+
 
 export { HistoryManager };
