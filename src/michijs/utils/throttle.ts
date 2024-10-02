1b3e1c3ebbd8c4aef0fad1dda719fc07883b5819
Lucas Segurado <lsegurado1996@gmail.com>
2024-10-02 20:27:02 +0200

    Added throttle

---
+++ src/michijs/utils/throttle.ts
@@ -0,0 +1,10 @@
+export const throttle = <T extends (...args: any[]) => void>(func: T, limit: number) => {
+  let inThrottle: boolean;
+  return (...args: Parameters<T>) => {
+    if (!inThrottle) {
+      func(...args);
+      inThrottle = true;
+      setTimeout(() => (inThrottle = false), limit);
+    }
+  };
+}
