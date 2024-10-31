325f76720b22573ba8041f5d89b22964da412a7b
Lucas Segurado <lsegurado1996@gmail.com>
2024-10-31 23:05:01 +0100

    Removed getRootNode

--- src/michijs/DOM/getRootNode.ts
+++
@@ -1,6 +0,0 @@
-import type { MichiCustomElement } from "../types";
-import { getShadowRoot } from "../utils/getShadowRoot";
-
-export function getRootNode(self: MichiCustomElement): Document | ShadowRoot {
-  return getShadowRoot(self) ?? (self.getRootNode() as Document | ShadowRoot);
-}
