38278fba5d4e833df30f1a6b93fd7a2ccd757a12
Lucas Segurado <lsegurado1996@gmail.com>
2025-06-30 14:43:51 +0200

    Small bugfix

--- src/michijs/typeWards/isCSSVariable.ts
+++ src/michijs/typeWards/isCSSVariable.ts
@@ -1,5 +1,4 @@
 import { Observable } from "../classes";
 import type { CSSVar } from "../types";
 
-export const isCSSVariable = (value: any): value is CSSVar<string> =>
-  typeof value === "function" && !(value instanceof Observable);
+export const isCSSVariable = (value: any): value is CSSVar<string> => typeof value === "function" && !value.compute;
