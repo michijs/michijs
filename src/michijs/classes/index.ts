d0ece973dce6685935eefcb76e9b38ecfe46d274
Lucas Segurado <lsegurado1996@gmail.com>
2025-07-13 13:35:36 +0200

    Exposed Callable

--- src/michijs/classes/index.ts
+++ src/michijs/classes/index.ts
@@ -1,5 +1,5 @@
 export { ProxiedValue } from "./ProxiedValue";
-export { CallableObservable, Observable } from "./Observable";
+export { Callable, CallableObservable, Observable } from "./Observable";
 export { CookieStorage } from "./CookieStorage";
 export { EventDispatcher } from "./EventDispatcher";
 export { ObservableFromEventListener } from "./ObservableFromEventListener";
