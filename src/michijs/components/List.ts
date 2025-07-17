6f8e0312ba6e658c9de1cf02a11d006ed7fc74ef
Lucas Segurado <lsegurado1996@gmail.com>
2025-07-17 13:20:16 +0200

    Small bugfixes

--- src/michijs/components/List.ts
+++ src/michijs/components/List.ts
@@ -1,22 +1,24 @@
-import type { ProxiedArray } from "../classes";
-import type { ElementFactoryType, FC, ObservableTypeOrConst } from "../types";
+import { GarbageCollectableObject, VirtualFragment } from "../classes";
+import { create } from "../DOM/create/create";
+import type { ElementFactoryType, FC, ObservablePrimitiveType, ObservableType, ObservableTypeOrConst, ObservableArray } from "../types";
 import { isObservable } from "../typeWards/isObservable";
+import { bindObservable } from "../utils";
 
 /**
  * Props for the List component.
  *
  * @template T - The type of items in the data array.
  */
-interface ListComponentProps<T> {
+interface ListComponentProps<T extends ObservableTypeOrConst<any[]>> {
   /**
    * The data source, which can be a regular array or an observable array.
    */
-  data: ObservableTypeOrConst<T[]>;
+  data: T;
 
   /**
    * A function that renders each item in the list.
    */
-  renderItem: FC<T>;
+  renderItem: FC<[T] extends [ObservableArray<(infer Y)>] ? ObservableType<Y> : ([T] extends [ObservablePrimitiveType<(infer Z)[]>] ? Z: T[any])>;
 }
 
 /**
@@ -30,13 +32,19 @@ interface ListComponentProps<T> {
  * @param factory - The element factory to use
  * @returns The rendered list, either by using the observable's `.List` method or via a direct map.
  */
-export const List = <const T = unknown>(
-  props: ListComponentProps<T>,
+export const List = <const T extends ObservableTypeOrConst<any[]>>(
+  { data, renderItem }: ListComponentProps<T>,
   factory: ElementFactoryType,
 ) => {
-  if (isObservable(props.data)) {
-    const data = props.data as unknown as ProxiedArray<T>;
-    return data.List({ renderItem: props.renderItem }, factory);
+  if (isObservable(data)) {
+    if ("List" in data)
+      return data.List({ renderItem }, factory);
+    else {
+      const el = new VirtualFragment();
+      const gc = new GarbageCollectableObject(el);
+      bindObservable(data, (data) => gc.ref.append(...(data as ObservablePrimitiveType<any[]>)().map(x => create(renderItem(x, factory), factory.contextElement))))
+      return el;
+    }
   }
-  return props.data.map((x) => props.renderItem(x, factory));
+  return data.map((x) => renderItem(x, factory));
 };
