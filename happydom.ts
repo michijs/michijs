499378b3ac61ce696193f84e6bcf0d26b270f321
Lucas Segurado <lsegurado1996@gmail.com>
2025-08-04 12:11:46 +0200

    Temporally disabled "benchmark-michijs" test

--- happydom.ts
+++ happydom.ts
@@ -1,17 +1,3 @@
 import { GlobalRegistrator } from "@happy-dom/global-registrator";
 
 GlobalRegistrator.register();
-
-// Temporary fix until happy-dom gets updated
-// Add your globals
-const globalAny = globalThis as any;
-
-// Add your globals
-globalAny.cookieStore = {
-  getAll(){
-    return []
-  },
-  addEventListener(){
-    
-  }
-};
