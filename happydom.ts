eb40ecd7a392828a02e636cd2b7a43a4caf245d1
Lucas Segurado <lsegurado1996@gmail.com>
2025-08-04 12:09:46 +0200

    Test bugfix

--- happydom.ts
+++ happydom.ts
@@ -1,3 +1,17 @@
 import { GlobalRegistrator } from "@happy-dom/global-registrator";
 
 GlobalRegistrator.register();
+
+// Temporary fix until happy-dom gets updated
+// Add your globals
+const globalAny = globalThis as any;
+
+// Add your globals
+globalAny.cookieStore = {
+  getAll(){
+    return []
+  },
+  addEventListener(){
+    
+  }
+};
