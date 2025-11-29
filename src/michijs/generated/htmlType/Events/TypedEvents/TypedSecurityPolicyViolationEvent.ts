d523692f448931a53cd9c221485e4edfb26aa83d
Lucas Segurado <lsegurado1996@gmail.com>
2025-11-29 22:37:54 +0100

    Bump HTML types (#597)
    * Changes requested by linter - some fixes
    
    * Small bugfix
    
    * Small bugfix
    
    * Small fix
    
    * Small fix
    
    * [develop] Autofix changes (#595)
    
    * chore: Update biome to 0.0.30
    
    * chore: Autofixes
    
    ---------
    
    Co-authored-by: michijs[bot] <235342604+michijs[bot]@users.noreply.github.com>
    
    * Small change
    
    * Small change
    
    * Bump
    
    * Test downgrade
    
    * chore: Update biome to 0.0.30
    
    * chore: Release 2.3.28-beta.0
    
    * Bump
    
    * chore: Update biome to 0.0.30
    
    * chore: Release 2.3.28-beta.1
    
    ---------
    
    Co-authored-by: michijs[bot] <235342604+michijs[bot]@users.noreply.github.com>

--- src/michijs/generated/htmlType/Events/TypedEvents/TypedSecurityPolicyViolationEvent.ts
+++ src/michijs/generated/htmlType/Events/TypedEvents/TypedSecurityPolicyViolationEvent.ts
@@ -1,15 +1,15 @@
 import type { TypedEvent } from "./TypedEvent";
 export interface TypedSecurityPolicyViolationEvent<T> extends TypedEvent<T> {
-  readonly blockedURI: string;
-  readonly columnNumber: number;
-  readonly disposition: SecurityPolicyViolationEventDisposition;
-  readonly documentURI: string;
-  readonly effectiveDirective: string;
-  readonly lineNumber: number;
-  readonly originalPolicy: string;
-  readonly referrer: string;
-  readonly sample: string;
-  readonly sourceFile: string;
-  readonly statusCode: number;
-  readonly violatedDirective: string;
+    readonly blockedURI: string;
+    readonly columnNumber: number;
+    readonly disposition: SecurityPolicyViolationEventDisposition;
+    readonly documentURI: string;
+    readonly effectiveDirective: string;
+    readonly lineNumber: number;
+    readonly originalPolicy: string;
+    readonly referrer: string;
+    readonly sample: string;
+    readonly sourceFile: string;
+    readonly statusCode: number;
+    readonly violatedDirective: string;
 }
