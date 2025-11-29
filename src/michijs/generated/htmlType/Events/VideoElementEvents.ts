2f0d2ea84cc05045ce2a18a04b6ae5d0482352da
Lucas Segurado <lsegurado1996@gmail.com>
2025-11-29 22:10:46 +0100

    Bump

--- src/michijs/generated/htmlType/Events/VideoElementEvents.ts
+++ src/michijs/generated/htmlType/Events/VideoElementEvents.ts
@@ -1,6 +1,3 @@
 import type { AllEvents } from "./AllEvents";
-export interface VideoElementEvents<T extends EventTarget>
-  extends Pick<
-    AllEvents<T>,
-    "onenterpictureinpicture" | "onleavepictureinpicture"
-  > {}
+export interface VideoElementEvents<T extends EventTarget> extends Pick<AllEvents<T>, "onenterpictureinpicture" | "onleavepictureinpicture"> {
+}
