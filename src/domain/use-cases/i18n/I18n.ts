7aef79ed5ce6d7c27192cf612027b2c8d70c0c1d
Lucas Segurado <lsegurado1996@gmail.com>
2026-03-20 15:52:08 +0100

    Fixed i18n

--- src/domain/use-cases/i18n/I18n.ts
+++ src/domain/use-cases/i18n/I18n.ts
@@ -72,7 +72,8 @@ export class I18n<K extends string = string> extends ReactiveValue<K> {
     return useAsyncComputedObserve<Partial<T>>(
       async () => await this.getCurrentTranslation(translation),
       translation[this.defaultLanguage] as Partial<T>,
-      [this]
+      [this],
+      { useProxied: true }
     );
   }
 
