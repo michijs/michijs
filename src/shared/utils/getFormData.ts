57e1c4a5828ad8f46864641f0bd813bd725e5887
Lucas Segurado <lsegurado1996@gmail.com>
2026-05-19 13:50:51 +0200

    Improved form management

--- src/shared/utils/getFormData.ts
+++ src/shared/utils/getFormData.ts
@@ -6,7 +6,18 @@ export const getFormData = <T extends object>(
       ? formOrEvent.target
       : formOrEvent
   ) as HTMLFormElement;
-  return Object.fromEntries(
-    new FormData(form) as unknown as Iterable<readonly [PropertyKey, any]>,
-  ) as T;
+  const flat = Object.fromEntries(new FormData(form));
+
+  return Object.entries(flat).reduce((acc, [key, value]) => {
+    const parts = key.split(".");
+    parts.reduce((obj, part, i) => {
+      if (i === parts.length - 1) {
+        obj[part] = value;
+      } else {
+        obj[part] ??= {};
+      }
+      return obj[part];
+    }, acc);
+    return acc;
+  }, {});
 };
